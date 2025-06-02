42e1001fccc11f46e37b39a7303e7c19f90decac
PandulceWeB <panaderiaweb1@gmail.com>
2025-06-01 23:38:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,15 +4,19 @@ const pages = document.querySelectorAll('.page');
 function nextPage() {
     if (currentPage < pages.length - 1) {
         pages[currentPage].classList.remove('active');
+        pages[currentPage].style.transform = "rotateY(-90deg)"; // Efecto de pase de hoja
         currentPage++;
         pages[currentPage].classList.add('active');
+        pages[currentPage].style.transform = "rotateY(0deg)";
     }
 }
 
 function prevPage() {
     if (currentPage > 0) {
         pages[currentPage].classList.remove('active');
+        pages[currentPage].style.transform = "rotateY(90deg)"; // Efecto de pase de hoja
         currentPage--;
         pages[currentPage].classList.add('active');
+        pages[currentPage].style.transform = "rotateY(0deg)";
     }
 }
