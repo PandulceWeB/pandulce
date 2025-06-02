502aa5d2c0a696ae795226684c68e3e7706250d4
PandulceWeB <panaderiaweb1@gmail.com>
2025-06-02 00:24:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,27 +4,27 @@ const pages = document.querySelectorAll('.page');
 function nextPage() {
     if (currentPage < pages.length - 1) {
         pages[currentPage].classList.remove('active');
-        pages[currentPage].style.transform = "rotateY(-180deg)"; // Efecto de pase de hoja
+        pages[currentPage].style.transform = "rotateY(-180deg) scaleX(0)"; // Efecto de pase de hoja
         setTimeout(() => {
             pages[currentPage].style.display = "none";
             currentPage++;
             pages[currentPage].style.display = "flex";
             pages[currentPage].classList.add('active');
-            pages[currentPage].style.transform = "rotateY(0deg)";
-        }, 800);
+            pages[currentPage].style.transform = "rotateY(0deg) scaleX(1)";
+        }, 600);
     }
 }
 
 function prevPage() {
     if (currentPage > 0) {
         pages[currentPage].classList.remove('active');
-        pages[currentPage].style.transform = "rotateY(180deg)"; // Efecto de pase de hoja
+        pages[currentPage].style.transform = "rotateY(180deg) scaleX(0)"; // Efecto de pase de hoja
         setTimeout(() => {
             pages[currentPage].style.display = "none";
             currentPage--;
             pages[currentPage].style.display = "flex";
             pages[currentPage].classList.add('active');
-            pages[currentPage].style.transform = "rotateY(0deg)";
-        }, 800);
+            pages[currentPage].style.transform = "rotateY(0deg) scaleX(1)";
+        }, 600);
     }
 }
