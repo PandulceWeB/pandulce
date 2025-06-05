f1176c4c35101c97916f7f60a1311bf1ff2711b8
PandulceWeB <panaderiaweb1@gmail.com>
2025-06-05 19:57:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,15 +5,10 @@ function nextPage() {
     if (currentPage < pages.length - 1) {
         pages[currentPage].classList.add("page-turning");
         setTimeout(() => {
-            pages[currentPage].classList.remove("active");
-            pages[currentPage].classList.remove("page-turning");
+            pages[currentPage].classList.remove("active", "page-turning");
             currentPage++;
             pages[currentPage].classList.add("active");
-            pages[currentPage].classList.add("page-entering");
-            setTimeout(() => {
-                pages[currentPage].classList.remove("page-entering");
-            }, 500);
-        }, 500);
+        }, 600);
     }
 }
 
@@ -21,14 +16,9 @@ function prevPage() {
     if (currentPage > 0) {
         pages[currentPage].classList.add("page-turning");
         setTimeout(() => {
-            pages[currentPage].classList.remove("active");
-            pages[currentPage].classList.remove("page-turning");
+            pages[currentPage].classList.remove("active", "page-turning");
             currentPage--;
             pages[currentPage].classList.add("active");
-            pages[currentPage].classList.add("page-entering");
-            setTimeout(() => {
-                pages[currentPage].classList.remove("page-entering");
-            }, 500);
-        }, 500);
+        }, 600);
     }
 }
